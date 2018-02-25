a8d473e25b660002b6b8aeb4c67d8db2e7a9d26e
Iulia Harasim <iulia.harasim@gmail.com>
2018-02-25 11:33:40 +0200

    Updated semester info

--- src/main/java/utils/SemesterInfo.java
+++ src/main/java/utils/SemesterInfo.java
@@ -5,12 +5,12 @@ public class SemesterInfo {
     public static final int CHRISTMAS_HOLIDAY = 12;
     public static final int CHRISTMAS_HOLIDAY_LENGTH = 2;
 
-    public static final int NO_OF_WEEKS_SECOND_SEMESTER = 15;
-    public static final int EASTER_HOLIDAY = 7;
+    public static final int NO_OF_WEEKS_SECOND_SEMESTER = 13;
+    public static final int EASTER_HOLIDAY = 6;
     public static final int EASTER_HOLIDAY_LENGTH = 1;
     
     public static final String STARTING_DATE_FIRST_SEMESTER = "2017-10-02";
-    public static final String STARTING_DATE_SECOND_SEMESTER = "2017-02-27";
+    public static final String STARTING_DATE_SECOND_SEMESTER = "2018-02-26";
 
     public static int getNoOfWeeks(int semester) {
         return (semester == 1 ? NO_OF_WEEKS_FIRST_SEMESTER : NO_OF_WEEKS_SECOND_SEMESTER);
