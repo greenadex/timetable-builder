14a291aba84d2b64825fc4ff03a408345d469f65
Sergiu Puscas <srg.pscs@gmail.com>
2018-04-11 19:15:23 +0300

    run.sh only builds if target does not exist.

--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
-gradle clean
-gradle build
-gradle fatJar
-java -jar build/libs/timetable-builder-1.0.jar $@
+#!/bin/bash
+
+target="build/libs/timetable-builder-1.0.jar"
+
+if [[ ! -e ${target} ]]; then
+  gradle clean
+  gradle build
+  gradle fatJar
+fi
+
+java -jar ${target} $@
